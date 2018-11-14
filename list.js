45362a9d77152a8bf5b6c3eef7ec894f05b5c822
Faris Ibrahim <faristheknight9@gmail.com>
2018-11-14 15:06:37 -0500

    Finished most of list class

--- list.js
+++ list.js
@@ -1,5 +1,51 @@
-class List {
+const Node = require("./node");
 
+class List {
+    constructor(ID, name) {
+        this.head = new Node(ID, 0);
+        this.name = name;
+    }
+
+    getIDs() {
+        let IDs = [];
+        let current = this.head.next;
+        while (current != null) {
+            IDs.push(current.ID);
+            current = current.next;
+        }
+        return IDs;
+    }
+
+    getCost(ID) {
+        let current = this.head;
+        while (current != null) {
+            if (current.ID == ID) {
+                return current.cost;
+            }
+            current = current.next;
+        }
+        return -1;
+    }
+
+    setCost(ID, cost) {
+        let current = this.head;
+        while (current != null) {
+            if (current.ID == ID) {
+                current.cost = cost;
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+    add(ID, cost) {
+        let current = this.head;
+        while(current.next != null) {
+            current = current.next;
+        }
+        current.next = new Node(ID, cost);
+    }
 }
 
 module.exports = List;
