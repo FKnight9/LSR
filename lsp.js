7e77b4f01d83bfa4e065fb51f056e64b8028ba98
Joey Kuhn <jkuhn@stevens.edu>
2018-12-04 05:14:58 -0500

    updated node & lsp

--- lsp.js
+++ lsp.js
@@ -1,5 +1,16 @@
 class LSP {
 
+    constructor(router, sequence, list, TTL) 
+    {
+        this.router = router;
+        this.sequence = sequence;
+        this.list = list.copy();
+        this.TTL = TTL;
+    }
+    copy() 
+    {
+        return new LSP(this.router, this.sequence, this.list, this.TTL);
+    }
 }
 
 module.exports = LSP;
--- node.js
+++ node.js
@@ -1,5 +1,18 @@
 class Node {
 
+    constructor (ID, cost) 
+    {
+        this.ID = ID;
+        if (cost != undefined) 
+        {
+            this.cost = cost;
+        } 
+        else 
+        {
+            this.cost = 1;
+        }
+        this.next = null;
+    }
 }
 
 module.exports = Node;
